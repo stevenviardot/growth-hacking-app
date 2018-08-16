11dd7eb49d0eb13d040b07a8600586df84ee116c
steven@forban.io <steven@MacBook-Air-de-viardot.local>
2018-08-16 22:23:32 +0200

    final

--- app/bot/instagram.rb
+++ app/bot/instagram.rb
@@ -2,27 +2,27 @@ require 'watir'
 require 'dotenv'
 Dotenv.load
 
-username = ENV["username"]
-password = ENV["password"]
-#Les personnes à  suivre
-user = "theweeknd"
+  username = ENV["username"]
+  password = ENV["password"]
+  #Les personnes à  suivre
+  user = "theweeknd"
 
-#Ouvertue de la page log in d'instagram
-browser = Watir::Browser.new :chrome
-browser.goto 'instagram.com/accounts/login/'
-# _5f5mN.jIbKX.KUBKM.yZn4P
+  #Ouvertue de la page log in d'instagram
+  browser = Watir::Browser.new :chrome
+  browser.goto 'instagram.com/accounts/login/'
+  # _5f5mN.jIbKX.KUBKM.yZn4P
 
-#Insertion des ID de l'utilsateur
-puts "Logging in..."
-browser.text_field(:name => "username").set "#{username}"
-browser.text_field(:name => "password").set "#{password}"
+  #Insertion des ID de l'utilsateur
+  puts "Logging in..."
+  browser.text_field(:name => "username").set "#{username}"
+  browser.text_field(:name => "password").set "#{password}"
 
-#Clique sur le bouton loggin
-browser.button(:class => "_5f5mN       jIbKX KUBKM      yZn4P   ").click
-sleep(2)
+  #Clique sur le bouton loggin
+  browser.button(:class => "_5f5mN       jIbKX KUBKM      yZn4P   ").click
+  sleep(2)
 
 
-#CLique sur le bouton Follow
-browser.goto "instagram.com/#{user}/"
-browser.button(:class => "_5f5mN       jIbKX  _6VtSN     yZn4P   ").click
-sleep(360)
+  #CLique sur le bouton Follow
+  browser.goto "instagram.com/#{user}/"
+  browser.button(:class => "_5f5mN       jIbKX  _6VtSN     yZn4P   ").click
+  sleep(360)
