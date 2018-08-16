1308e18db3a346cff30f93ba425a626dacc35162
steven@forban.io <steven@MacBook-Air-de-viardot.local>
2018-08-16 18:47:19 +0200

    mid range

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :models
+  get 'home/index'
+  root 'home#index'
+  get 'dashboard', to: 'home#dashboard', as: 'dash'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
